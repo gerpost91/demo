5862a5260fd9687f7e07ec135af0f9522cca3457
Gerardo Guevara <gerardoguevara800@gmail.com>
2020-07-05 19:17:11 -0600

    alistando componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { BreadcrumsComponent } from './shared/breadcrums/breadcrums.component';
+import { TableComponent } from './components/table/table.component';
+import { LoginComponent } from './login/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidebarComponent,
+    FooterComponent,
+    BreadcrumsComponent,
+    TableComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
